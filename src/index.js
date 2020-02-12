7fe35169831e23cfb9e8cbbf5ed1dc05bc943603
Eglesim <egle.asim@gmail.com>
2020-02-12 21:47:20 +0200

    structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+//import monitor from './monitor.png';
+import './index.hbs'
+
+
+let submit = () => {
+    let newComment = document.getElementById('new-comment').value;
+    let commentBox = document.getElementById('comment-box');
+    commentBox.innerHTML += `<p class='comment'>${newComment}</p>`;
+
+    commentBox.innerHTML += `<img class="img" src=${monitor} />`;
+}
+
+window.submit = submit;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    contentBase: path.join(__dirname, 'public'),
+    port: 3001
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      { test: /\.hbs$/, loader: "handlebars-loader" },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/images/[name].[ext]',
+        },
+      }
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        handlebarsLoader: {}
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'index',
+      filename: 'index.html',
+      template: './src/index.hbs'
+    }),
+  ]
+};
